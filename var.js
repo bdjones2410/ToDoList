ef52e2dd31133f7243f0a143f9d05102bd3e9eee
Brandon Jones <bdjones2410@gmail.com>
2015-10-27 14:07:06 -0400

    styled layout started making templates and javascript variables.

---
+++ var.js
@@ -0,0 +1,48 @@
+//////////////////
+// DATA ARRAY   //
+//////////////////
+
+var youDo = [
+  {
+    toDo : "Take out trash"
+  },
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////
+// TEMPLATES    //
+//////////////////
+var Templates= {
+ toDoFill:[
+            '<div class="toDolist" id="<%= idx %>">',
+            '<div class="checkBox">',
+            '<div id="check">&#x2713;</div>',
+            '</div>',
+            '<div class="toDoItem">',
+            '<h4><%= toDo =></h4>',
+            '</div>',
+            '</div>'
+            ].join(""),
+
+
+
+
+
+
+
+
+};
